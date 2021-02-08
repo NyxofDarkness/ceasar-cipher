4cd4047de7319ece3138aac21ecc46534bd1069a
NyxofDarkness <ibelieve.as@gmail.com>
2021-02-07 21:08:10 -0800

    test written but import error to deal with

--- tests/test_ceasar_cipher.py
+++ tests/test_ceasar_cipher.py
@@ -6,15 +6,22 @@ message = "Ny bfx ymj gjxy tk ynrjx, ny bfx ymj btwxy tk ynrjx."
 
 
 # encrypt a string with a given shift
+# encryption should handle upper and lower case letters
+# encryption should allow non-alpha characters but ignore them, including white space
 def test_encrypt():
     actual = encrypt(text)
     expected = message
     assert actual == expected
 
 # decrypt a previously encrypted string with the same shift
-# encryption should handle upper and lower case letters
-# encryption should allow non-alpha characters but ignore them, including white space
-# decrypt encrypted version of It was the best of times, it was the worst of times. WITHOUT knowing the shift used.
-# refer to supplied unit tests.
 def test_decrypt():
-    pass
+    actual = decrypt(message)
+    expected = text
+    assert actual == expected
+
+
+# decrypt encrypted version of It was the best of times, it was the worst of times. WITHOUT knowing the shift used.
+def test_crack():
+    actual = crack(message)
+    expected = text
+    assert actual == expected
